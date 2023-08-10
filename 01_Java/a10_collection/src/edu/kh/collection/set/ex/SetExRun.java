cafb471699978d693380d3bf8a6d74f7ae22c554
ELIA <ujiny1995@naver.com>
2023-08-10 12:32:41 +0900

    Set, Map

---
+++ 01_Java/a10_collection/src/edu/kh/collection/set/ex/SetExRun.java
@@ -0,0 +1,13 @@
+package edu.kh.collection.set.ex;
+
+public class SetExRun {
+	public static void main(String[] args) {
+		
+		SetEx example = new SetEx();
+		
+		example.ex3();
+		example.createLottoNumber();
+		example.lottoVersion2();
+		
+	}
+}
