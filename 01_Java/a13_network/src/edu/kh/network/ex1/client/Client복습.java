efe623fa79dad1517f361a62e09cd447149c883a
ELIA <ujiny1995@naver.com>
2023-08-17 17:44:48 +0900

    서버, 클라이언트

---
+++ 01_Java/a13_network/src/edu/kh/network/ex1/client/Client복습.java
@@ -0,0 +1,63 @@
+package edu.kh.network.ex1.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client복습 {
+
+	public void start() { 
+		
+		String ip = "192.168.10.19";
+		int port = 8500;
+		
+		Socket socket = null;
+		
+		InputStream is = null;
+		OutputStream os = null;
+		
+		BufferedReader br = null;
+		PrintWriter pw = null;
+		
+		try {
+			
+			socket = new Socket(ip, port);
+			
+			if(socket != null) {
+				is = socket.getInputStream();
+				os = socket.getOutputStream();
+				
+				br = new BufferedReader(new InputStreamReader(is));
+				pw = new PrintWriter(os);
+				
+				String serverMessage = br.readLine();
+				System.out.println(serverMessage);
+				
+				Scanner sc = new Scanner(System.in);
+				
+				System.out.println("서버로 전달할 메시지 : ");
+				String clientMessage = sc.nextLine();
+				
+				pw.println(clientMessage);
+				pw.flush();
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(br != null) br.close();
+				if(pw != null) pw.close();
+				
+				if(socket != null) socket.close();
+			}catch(IOException e	) {
+				e.printStackTrace();
+			}
+		}
+	
+	}
+}
---
+++ 01_Java/a13_network/src/edu/kh/network/ex1/server/Server복습.java
@@ -0,0 +1,82 @@
+package edu.kh.network.ex1.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Server복습 {
+
+	public void start() {
+		
+		int port = 8500;
+		
+		ServerSocket serverSocket = null;
+		Socket clientSocket = null;
+		
+		InputStream is = null;
+		OutputStream os = null;
+		
+		BufferedReader br = null;
+		PrintWriter pw = null;
+		
+		try {
+			InetAddress inet = InetAddress.getLocalHost();
+			System.out.println("서버 ip 주소 : " + inet.getHostAddress());
+			
+			serverSocket = new ServerSocket(port);
+			
+			System.out.println("서버입니다");
+			System.out.println("클라이언트 접속 대기중");
+			
+			clientSocket = serverSocket.accept();
+			
+			System.out.println("클라이언트 접속 성공");
+			
+			is = clientSocket.getInputStream();
+			os = clientSocket.getOutputStream();
+			
+			br = new BufferedReader(new InputStreamReader(is));
+			pw = new PrintWriter(os);
+			
+			Date now = new Date();
+			
+			SimpleDateFormat sdf = new SimpleDateFormat();
+			
+			String message = sdf.format(now);
+			
+			pw.println(message);
+			pw.flush();
+			
+			String clientMessage = br.readLine();
+			String clientIP = clientSocket.getInetAddress().getHostAddress();
+			
+			System.out.println(clientIP + "로부터 받은 메시지"+ clientMessage);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(is != null) is.close();
+				if(pw != null) pw.close(); 
+				
+				if(serverSocket != null) serverSocket.close();
+				if(clientSocket != null) clientSocket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	
+	
+	
+	
+}
