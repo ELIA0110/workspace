b107135e38a03410053b6963dec3cbb593d9afbe
ELIA <ujiny1995@naver.com>
2023-08-24 17:49:25 +0900

    조인, 쿼리

--- 02_DB/03_GROUP BY_HAVING.sql
+++ 02_DB/03_GROUP BY_HAVING.sql
@@ -234,7 +234,7 @@ WHERE DEPT_CODE IS NULL;
 -- 부서코드가 'D5' 또는 'D6'인 사원 이름, 부서코드 조회
 SELECT EMP_NAME, DEPT_CODE
 FROM EMPLOYEE
-WHERE DEPT_CODE 'D5'
+WHERE DEPT_CODE = 'D5'
 
 UNION
 
@@ -258,10 +258,67 @@ WHERE SALARY > 3000000;
 -- UNION ALL : 여러개의 쿼리 결과를 하나로 합치는 연산자
 -- UNION과의 차이점은 중복영역을 모두 포함시킨다. (합집합 +  교집합)
 
+-- 부서코드가 'D5'이거나 급여가 300만 초과하는 사원의 
+-- 이름, 부서코드, 급여 조회(중복 포함)
+SELECT EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D5'
 
+UNION ALL 
+
+SELECT EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE SALARY > 3000000;
 
 -- MINUS : 선행 SELECT 결과에서 다음 SELECT 결과와 겹치는 부분을 제외한 나머지 부분만 추출(차집합)
 -- 부서 코드 D5 중 급여가 300만 초과인 직원 제외
+SELECT EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D5'
+
+MINUS
+
+SELECT EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE SALARY > 3000000;
+
+
+------------------------------------------------------------
+
+-- 집합 연산자 실제 사용 예시
+
+-- * 집합 연산자 사용 시
+--   컬럼의 타입만 일치하면 연산 수행이 가능하다 *
+
+SELECT EMP_NAME, SALARY FROM EMPLOYEE
+WHERE EMP_ID = 200
+
+UNION 
+
+SELECT PHONE, 1000000 FROM EMPLOYEE
+WHERE EMP_ID = 201
+
+UNION
+
+SELECT '홍길동', 4000000 FROM DUAL;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
