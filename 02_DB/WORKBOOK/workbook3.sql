a081bb8b093918043f7de3289570d7012f05270e
ELIA <ujiny1995@naver.com>
2023-08-28 17:03:38 +0900

    DML, TCL, DDL

--- 02_DB/WORKBOOK/workbook3.sql
+++ 02_DB/WORKBOOK/workbook3.sql
@@ -82,6 +82,13 @@ ORDER BY CLASS_NO ;
 -- 10번
 -- 음악학과 학생들의 "학번", "학생 이름", "전체 평점"을 조회하시오.
 -- (단, 평점은 소수점 1자리까지만 반올림하여 표시한다.)
+SELECT STUDENT_NO, STUDENT_NAME, ROUND(AVG(POINT),1) "전체 평점"
+FROM TB_STUDENT
+JOIN TB_DEPARTMENT USING(DEPARTMENT_NO)
+JOIN TB_GRADE USING(STUDENT_NO)
+WHERE DEPARTMENT_NAME = '음악학과'
+GROUP BY STUDENT_NO, STUDENT_NAME
+ORDER BY STUDENT_NO;
 
 -- 11번
 -- 학번이 A313047인 학생의 학과이름, 학생이름, 지도교수 이름을 조회하시오.
@@ -93,8 +100,14 @@ ORDER BY CLASS_NO ;
 -- 13번
 -- 예체능 계열 과목 중 과목 담당교수를 한 명도 배정받지 못한 과목을 찾아
 -- 과목 이름, 학과 이름을 조회하시오.
+SELECT CLASS_NAME, DEPARTMENT_NAME
+FROM TB_CLASS
+LEFT JOIN TB_CLASS_PROFESSOR USING(CLASS_NO)
+-- 담당교수가 없는 사람도 나타내기 위해 LEFT JOIN
+JOIN TB_DEPARTMENT USING(DEPARTMENT_NO)
+WHERE CATEGORY = '예체능'
+AND PROFESSOR_NO IS NULL;
 
-...
 
 -- 14번
 -- 춘 기술대학교 서반아어학과 학생들의 지도교수를 게시하고자 한다.
@@ -104,8 +117,15 @@ ORDER BY CLASS_NO ;
 -- 15번
 -- 휴학생이 아닌 학생 중 평점이 4.0 이상인 학생을 찾아
 -- 학생의 학번, 이름, 학과, 이름, 평점을 조회하시오.
+SELECT STUDENT_NO, STUDENT_NAME, DEPARTMENT_NAME, AVG(POINT)
+FROM TB_STUDENT 
+JOIN TB_DEPARTMENT USING(DEPARTMENT_NO)
+JOIN TB_GRADE USING(STUDENT_NO)
+WHERE ABSENCE_YN != 'Y'
+GROUP BY STUDENT_NO, STUDENT_NAME, DEPARTMENT_NAME
+HAVING AVG(POINT) >= 4
+ORDER BY STUDENT_NO;
 
-...
 
 -- 16번
 -- 환경조경학과 전공과목들의 과목 별 평점을 조회하시오.
